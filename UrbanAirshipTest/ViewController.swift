7efc9a88c792daea43c5940b42811f718125070e
khou22 <breathometer@Computers-MacBook-Pro.local>
2016-06-20 16:11:52 -0700

    Created Xcode Project

---
+++ UrbanAirshipTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UrbanAirshipTest
+//
+//  Created by Breathometer on 6/20/16.
+//  Copyright © 2016 KevinHou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
